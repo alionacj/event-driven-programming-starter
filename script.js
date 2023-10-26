b0cce56437f02877b5d96c6064590ac8f1fcce9d
Leo Johnson <leochinonejohnson@gmail.com>
2023-10-26 14:51:49 -0500

    Make counters work! Re-enabled deletions and made the counter work backwards.

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 function onReady() {
   console.log('Yay!, onReady() got called when the page loaded.');
-
+  // we can select things
+  // document basically means html file
+  // querySelector will match 1 thing. the first thing
+  let header = document.querySelector('h1');
+console.log(header)
+header.classList.add('pink-text')
+header.textContent = 'HELLO FROM JAVASCRIPT'
 }
 
 // Will call onReady() when the page loads.
-onReady()
+onReady();
+
+function potatoClick() {
+  console.log('you clicked potato.');
+  let farm = document.getElementById('potatoesAndUnicorns');
+  farm.innerHTML += '<span onclick="deletePotato(event)">🥔</span>';
+}
+
+function unicornClick() {
+  console.log('you clicked unicorn.');
+  let ranch = document.getElementById('potatoesAndUnicorns');
+  ranch.innerHTML += `<span onclick="deleteUnicorn(event)">🦄</span>`;
+
+}
+
+function deletePotato(event) {
+  console.log('you clicked -A- potato.', event.target); //event.target is where the click happened
+  event.target.remove()
+}
+
+function deleteUnicorn(event) {
+  console.log('you clicked -A- unicorn', event.target);
+  event.target.remove()
+}
